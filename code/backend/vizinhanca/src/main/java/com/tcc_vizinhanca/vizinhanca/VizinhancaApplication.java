df33018abdaf8d1fabc45eb2d27947e60b9231ae
LEONARDO SCOTTI TOBIAS <25132821@dsn127m1242269.cfp127aluno.local>
2026-04-22 13:49:55 -0300

    Transferindo Back-end para o repositório

---
+++ code/backend/vizinhanca/src/main/java/com/tcc_vizinhanca/vizinhanca/VizinhancaApplication.java
@@ -0,0 +1,13 @@
+package com.tcc_vizinhanca.vizinhanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizinhancaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VizinhancaApplication.class, args);
+    }
+
+}
---
+++ code/backend/vizinhanca/src/test/java/com/tcc_vizinhanca/vizinhanca/VizinhancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc_vizinhanca.vizinhanca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizinhancaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
