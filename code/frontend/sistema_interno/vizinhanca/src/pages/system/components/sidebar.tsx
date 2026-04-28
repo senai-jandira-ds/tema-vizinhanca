9393d51651fabae6f6501dedda0909250e394cdb
ROGER RIBEIRO DE OLIVEIRA <25132662@CFP127ALUNO.LOCAL>
2026-04-28 16:55:45 -0300

    sidebar finalizada

--- code/frontend/sistema_interno/vizinhanca/src/pages/system/components/sidebar.tsx
+++ code/frontend/sistema_interno/vizinhanca/src/pages/system/components/sidebar.tsx
@@ -1,23 +1,76 @@
 import React from "react";
 import "./sidebar.module.css";
+import back from "../icons/backicon.svg";
+import home from "../icons/homeicon.svg";
+import people from "../icons/peopleicon.svg";
+import global from "../icons/globalicon.svg";
+import hands from "../icons/handsicon.svg";
+import settings from "../icons/settingsicon.svg";
+import shield from "../icons/shieldsecurityicon.svg";
+import help from "../icons/helpicon.svg";
 
 function Sidebar() {
-    
+
     return (
         <aside>
             <nav>
-                <nav>
                 <ul>
-                <li><a href=""><img src={"../icons/homeicon"} alt="" /> Voltar</a></li>
-                <li><a href=""><img src={"../icons/homeicon"} alt="" />Home</a></li>
-                <li><a href=""><img src={"../icons/homeicon"} alt="" />Atividade Geral</a></li>
-                <li><a href=""><img src={"../icons/homeicon"} alt="" />Condôminos</a></li>
-                <li><a href=""><img src={"../icons/homeicon"} alt="" />Pedidos e Objetos</a></li>
-                <li><a href=""><img src={"../icons/homeicon"} alt="" />Denúncias</a></li>
-                <li><a href=""><img src={"../icons/homeicon"} alt="" />Configurações do Condomínio</a></li>
-                <li><a href=""><img src={"../icons/homeicon"} alt="" />Ajuda</a></li>
+                    <li>
+                        <a href="">
+                            <img src={back} alt="" />
+                            <span className="label">Voltar</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <img src={home} alt="" />
+                            <span className="label">Dashboard</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <img src={global} alt="" />
+                            <span className="label">Global</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <img src={people} alt="" />
+                            <span className="label">Usuários</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <img src={hands} alt="" />
+                            <span className="label">Serviços</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <img src={shield} alt="" />
+                            <span className="label">Segurança</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <img src={settings} alt="" />
+                            <span className="label">Configurações</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <img src={help} alt="" />
+                            <span className="label">Ajuda</span>
+                        </a>
+                    </li>
                 </ul>
-                </nav>
             </nav>
         </aside>
     )
