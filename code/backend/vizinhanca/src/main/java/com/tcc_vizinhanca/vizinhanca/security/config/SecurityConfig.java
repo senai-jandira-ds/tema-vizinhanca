2f475951d3ddf0e751113091fde44caa685b7f76
LEONARDO-SCOTTI <leonardo.scotti07@gmail.com>
2026-05-08 21:25:12 -0300

    Adicionando Swagger.

--- code/backend/vizinhanca/src/main/java/com/tcc_vizinhanca/vizinhanca/security/config/SecurityConfig.java
+++ code/backend/vizinhanca/src/main/java/com/tcc_vizinhanca/vizinhanca/security/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/condominium").permitAll()
                         .anyRequest().authenticated()
