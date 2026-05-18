9326f6d1c90aa13883c10175d5527b611533311e
leonardo-scotti <leonardo.scotti07@gmail.com>
2026-05-18 20:34:28 -0300

    Service da categoria.

--- code/backend/vizinhanca/src/main/java/com/tcc_vizinhanca/vizinhanca/service/category/CategoryService.java
+++ code/backend/vizinhanca/src/main/java/com/tcc_vizinhanca/vizinhanca/service/category/CategoryService.java
@@ -11,6 +11,7 @@ package com.tcc_vizinhanca.vizinhanca.service.category;
 import com.tcc_vizinhanca.vizinhanca.entity.category.Category;
 import com.tcc_vizinhanca.vizinhanca.repository.category.CategoryRepository;
 import lombok.NonNull;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -33,5 +34,31 @@ public class CategoryService {
     public Category getSelectCategoryById(@NonNull Long id) {
         return categoryRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada!"));
+    }
+
+    // INSERT CATEGORY
+    public Category setInsertCategory(@NonNull Category category) {
+        category.setId(null);
+
+        return categoryRepository.save(category);
+    }
+
+    // UPDATE CATEGORY
+    public Category setUpdateCategory(@NonNull Category category, Long idCategory) {
+        Category existingCategory = getSelectCategoryById(idCategory);
+
+        BeanUtils.copyProperties(
+                category, existingCategory, "id"
+        );
+
+        return categoryRepository.save(existingCategory);
+    }
+
+    // DELETE CONDOMINIUM
+    public void setDeleteCategory(Long idCategory) {
+        if (!categoryRepository.existsById(idCategory))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada!");
+
+        categoryRepository.deleteById(idCategory);
     }
 }
