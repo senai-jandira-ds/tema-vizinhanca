01b1183e09c04199e88db5bc73af5684da578db2
Leonardo-Scotti <scottileonardo1@gmail.com>
2026-05-27 20:18:17 -0300

    Adicionando configurações do Web Socket.

---
+++ code/backend/vizinhanca/src/main/java/com/tcc_vizinhanca/vizinhanca/config/WebSocketConfig.java
@@ -0,0 +1,42 @@
+/***************************************************
+ * Objetivo: Configuração do WebSocket com STOMP e SockJS
+ * Data: 27/05/2026
+ * Autor: Leonardo Scotti
+ * Versão: 1.0.05.26
+ * *************************************************/
+
+package com.tcc_vizinhanca.vizinhanca.config;
+
+import com.tcc_vizinhanca.vizinhanca.security.websocket.WebSocketAuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private WebSocketAuthInterceptor webSocketAuthInterceptor;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(webSocketAuthInterceptor);
+    }
+}
